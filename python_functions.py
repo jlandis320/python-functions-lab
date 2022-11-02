5589fffc61ea7dc030abc58a58941a724a46cb6d
jlandis320 <jreynoldslandis@gmail.com>
2022-11-02 11:27:38 -0400

    initial commit

---
+++ python_functions.py
@@ -0,0 +1,8 @@
+# 1. Write a function named `sum_to` that accepts a single integer, `n`, and returns the sum of the integers from 1 to `n`.
+    
+#     For example:
+# sum_to(6)  # returns 21
+# sum_to(10) # returns 55
+
+def sum_to(n):
+  return 
