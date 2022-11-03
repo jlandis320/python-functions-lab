c2551951929c8c8c51d5a53a33109b090bbdf8cb
jlandis320 <jreynoldslandis@gmail.com>
2022-11-02 20:16:24 -0400

    solves question 3

--- python_functions.py
+++ python_functions.py
@@ -8,4 +8,32 @@ def sum_to(n):
   result = sum(range(n + 1))
   print(result)
 
-sum_to(10)
+# sum_to(10)
+
+# 2. Write a function named `largest` that takes a list of numbers as an argument and returns the largest number in that list.
+    
+#     For example:
+#     largest([1, 2, 3, 4, 0])  # returns 4
+#     largest([10, 4, 2, 231, 91, 54])  # returns 231
+
+
+def largest(nums):
+  largest = 0
+  for num in nums:
+    if num > largest:
+      largest = num
+  return largest
+
+# Write a function named occurrences that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
+# For example:
+
+      # occurrences('fleep floop', 'e')   # returns 2
+      # occurrences('fleep floop', 'p')   # returns 2
+      # occurrences('fleep floop', 'ee')  # returns 1
+      # occurrences('fleep floop', 'fe')  # returns 0
+
+
+def occurrences(str1, str2):
+  return str1.count(str2)
+
+print(occurrences('fleep floop', 'fe'))
